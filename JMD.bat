7f79c6ca3d4556622f4ef815c7f41788be3bd58e
snoopythe3 <masonkember1+github@gmail.com>
2025-06-27 18:15:59 +0100

    Main JMD

---
+++ JMD.bat
@@ -0,0 +1,8 @@
+@echo off
+title Fake Command Prompt
+cls
+color 2
+echo Microsoft Windows [Version 10.0.22631.5039]
+echo (c) Microsoft Corporation. All rights reserved.
+echo This is a joke. You cannot interact with this.
+pause
